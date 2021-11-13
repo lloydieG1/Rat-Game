17a7d4d4d2fd21fcca6e60e9e307dd12b7564028
randleog <willdatank7@gmail.com>
2021-11-13 15:16:43 +0000

    added code to launch non-existant fxml

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,47 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Game {
+/**
+ * launching application which runs the game
+ * @author William Randle, Jack Lennard
+ *
+ */
+public class Game extends Application {
+
+
+
+    public void start(Stage primaryStage) {
+
+        BorderPane menuPane = null;
+        Scene menu = null;
+        try {
+            menuPane = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+            menu = new Scene(menuPane,1350,900);
+            primaryStage.setScene(menu);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        primaryStage.setScene(menu);
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 
 }
