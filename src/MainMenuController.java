913a236a328fe9b308a2781478bbed0081cf542f
randleog <willdatank7@gmail.com>
2021-11-15 20:53:55 +0000

    motd text now changable on start

--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -1,12 +1,18 @@
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * this class handles interactions with the main menu
  * @author William Randle
  */
-public class MainMenuController {
+public class MainMenuController implements Initializable {
 
-
+    @FXML
+    Text motd;
     /**
      * switches to level menu when level button is clicked
      */
@@ -16,6 +22,10 @@ public class MainMenuController {
 
         }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        motd.setText("lolk");
+    }
 }
 
 
