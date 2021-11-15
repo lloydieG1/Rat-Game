8f34925f9375cc7455fbd8a8601713f6d3c4eeb6
randleog <willdatank7@gmail.com>
2021-11-15 22:23:36 +0000

    improvements to DailyMessage

---
+++ src/DailyMessage.java
@@ -0,0 +1,91 @@
+import java.io.IOException;
+import java.net.*;
+import java.io.*;
+
+/**
+ * this class fetches the message of the day
+ * @author Jack
+ */
+public class DailyMessage {
+
+    /**
+     * gets the unsolved code used to retrieve the motd
+     * @param args
+     * @return String unsolved code
+     * @throws IOException
+     */
+	private static String getURLstring(String args[]) throws IOException {
+		
+		URL test = new URL ("http://cswebcat.swansea.ac.uk/puzzle");
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(test.openStream()));
+		
+		String inputLine;
+		if ((inputLine = in.readLine()) != null) {
+            return inputLine;
+        } else {
+            return "something ain't right";
+        }
+	}
+
+
+    /** solves the code from parsed unsolved code, which is then
+     * used to get the message of the day
+     *
+     * @param args
+     * @return String code to get message of the day
+     * @throws IOException
+     */
+    private static String translateURLstring(String args[]) throws IOException {
+        char[] cipher = getURLstring(null).toCharArray();
+        String convert = "";
+        for (int i = 1; i <= cipher.length; i++) {
+            int selectedChar = cipher[i-1];
+            if ((selectedChar > 90) || (selectedChar < 65)) {
+                convert = convert + cipher[i-1];
+            } else if (i % 2 == 0) {
+                selectedChar += i;
+                if (selectedChar > 90) {
+                    selectedChar = cipher[i-1] + (i) - 26;
+                }
+                convert = convert + (char) selectedChar;
+
+            } else {
+                selectedChar -= i;
+                if (selectedChar < 65) {
+                    selectedChar = cipher[(i-1)] + 26 - (i);
+                }
+                convert = convert + (char) selectedChar;
+            }
+
+
+        }
+        convert = convert + ("CS-230");
+        convert = (convert.length()+""+convert);
+        return convert;
+
+    }
+
+
+    /**
+     * returns the message of the day as a string
+     * @param args
+     * @return String message of the day
+     * @throws IOException
+     */
+	public static String getMessage(String args[]) throws IOException {
+		String url = "http://cswebcat.swansea.ac.uk/message?solution=".concat(translateURLstring(args));
+        System.out.println(translateURLstring(args));
+		URL test = new URL (url);
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(test.openStream()));
+		String inputLine;
+		if ((inputLine = in.readLine()) != null) {
+            return inputLine;
+        }
+		else {
+            return "something ain't right";
+        }
+	}
+
+}
--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -8,12 +8,12 @@ import java.util.ResourceBundle;
 
 /**
  * this class handles interactions with the main menu
- * @author William Randle
+ * @author William Randle, Jack
  */
 public class MainMenuController implements Initializable {
 
     @FXML
-    Text motd;
+    Text motd; //text on screen for message of the day
     /**
      * switches to level menu when level button is clicked
      */
@@ -23,10 +23,17 @@ public class MainMenuController implements Initializable {
 
         }
 
+
+
+    /**
+     * code ran on initalization, displays updated message of the day
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-			motd.setText(dailyMessage.getMessage(null));
+			motd.setText(DailyMessage.getMessage(null));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
