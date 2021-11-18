96f78e2a49d0b68c33f2275ee2590dc7153ccae6
LoriT0T <mesadhq@hotmail.com>
2021-11-18 08:06:44 +0000

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,20 +1,20 @@
 public class Tile extends Element {
-	private int width;
-	private int height;
+	private int xPos;
+	private int yPos;
 	private char tileType;
 	
-	public Tile (int width, int height, char tileType) {
-		this.width = width;
-		this.height = height;
+	public Tile (int xPos, int yPos, char tileType) {
+		this.xPos = xPos;
+		this.yPos = yPos;
 		this.tileType = tileType;
 	}
 	
-	public int getRow () {
-		return width;
+	public int getXPos() {
+		return xPos;
 	}
 	
-	public int getColumn() {
-		return height;
+	public int getYPos() {
+		return yPos;
 	}
 	
 	public char getTileType() {
