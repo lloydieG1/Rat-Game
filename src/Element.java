228a6a5a23a491d88de6732251b696ce6f5afe0f
randleog <willdatank7@gmail.com>
2021-11-26 15:11:38 +0000

    mvoement now more general
    (later on i would like to move movement out of rat though)

--- src/Element.java
+++ src/Element.java
@@ -91,6 +91,152 @@ public abstract class Element {
         return isFlagRemoval();
     }
 
+
+    /**
+     * handles the movement of the rat
+     */
+    protected void movement() {
+        x = nextX;
+        y = nextY;
+        //update to the position decided last tick.
+
+        int checkX = 0;
+        int checkY = 0;
+
+        int randomNum = Game.random.nextInt(4);
+
+        // changes direction until nolonger stuck
+        while (isStuck()) {
+            dir = rightDir(dir);
+        }
+
+        if (randomNum == 0) {
+            checkX = x;
+            checkY = y +1;
+        } else if (randomNum == 1) {
+            checkX = x+1;
+            checkY = y;
+        }else if (randomNum == 2) {
+            checkX = x;
+            checkY = y-1;
+        }else if (randomNum == 3) {
+            checkX = x-1;
+            checkY = y;
+        }
+
+        while (!(isTile(checkX, checkY))) {
+            randomNum = Game.random.nextInt(4);
+
+            if (randomNum == 0) {
+                checkX = x;
+                checkY = y +1;
+            } else if (randomNum == 1) {
+                checkX = x+1;
+                checkY = y;
+            }else if (randomNum == 2) {
+                checkX = x;
+                checkY = y-1;
+            }else if (randomNum == 3) {
+                checkX = x-1;
+                checkY = y;
+            }
+        }
+
+        if (checkY == y+1) {
+            dir = Direction.North;
+        } else if (checkY == y-1) {
+            dir = Direction.South;
+        }else if (checkX == x+1) {
+            dir = Direction.East;
+        }else if (checkX == x-1) {
+            dir = Direction.West;
+        }
+
+        this.nextX = checkX;
+        this.nextY = checkY;
+
+
+    }
+
+    /**
+     *  checks if the tile at parsed position is a eligable tile to go onto
+     *
+     * @return boolean if tile is safe
+     */
+    protected boolean isTile(int x, int y) {
+        int boardSize = level.getLength();
+
+        if (x < 0 || y < 0) {
+            return false;
+        }
+
+        if (x > boardSize-1 || y > boardSize-1) {
+            return false;
+        }
+
+        if (dir.equals(Direction.North) && y == this.y-1 ) {
+            return false;
+        } else if (dir.equals(Direction.East) && x == this.x-1 ) {
+            return false;
+        }else if (dir.equals(Direction.South) && y == this.y+1 ) {
+            return false;
+        }else if (dir.equals(Direction.West) && x == this.x+1 ) {
+            return false;
+        }
+
+        if (level.getTile(x, y).getType().equals(TileType.Grass)) {
+            return false;
+        }
+
+
+        return true;
+    }
+
+    /**
+     * returns true if the rat cannot move to a nearby tile
+     * @return
+     */
+    protected boolean isStuck() {
+        int checkX= x;
+        int checkY = y;
+        int totalPaths = 0; //if paths accesable is < 2 then stuck
+
+        if(isTile(checkX, checkY+1)) {
+            totalPaths++;
+        }
+
+        if(isTile(checkX+1, checkY)) {
+            totalPaths++;
+        }
+
+        if(isTile(checkX, checkY-1)) {
+            totalPaths++;
+        }
+
+        if(isTile(checkX-1, checkY)) {
+            totalPaths++;
+        }
+
+        return totalPaths<1;
+
+
+
+
+    }
+
+    //returns dir to the right
+    protected Direction rightDir(Direction dir) {
+        if (dir.equals(Direction.North)) {
+            return Direction.East;
+        } else if (dir.equals(Direction.East)) {
+            return Direction.South;
+        } else if (dir.equals(Direction.South)) {
+            return Direction.West;
+        } else {
+            return Direction.North;
+        }
+    }
+
     /**
      * smoothly interpolates number frames
      * @param x1
