63a2943ef6854f609c966746fa0935ebc6c684cd
LoriT0T <mesadhq@hotmail.com>
2021-11-27 18:50:53 +0000

    Update Rat.java

--- src/Rat.java
+++ src/Rat.java
@@ -10,17 +10,13 @@ public class Rat extends Element {
 
     private boolean isMale;
 
-    private boolean isPregnant;
-
     private static int ADULTHOOD = 5;
-
-    private boolean isSterile;
 
     private boolean isChild;
 
     private int breeding = 0;
-    private final int BREEDING_TIME = 3;
-
+    
+    private final int BREEDING_TIME = 5;
 
 
     
@@ -36,11 +32,8 @@ public class Rat extends Element {
      */
     public Rat(ElementType type, Level level, int x, int y, boolean isMale, Direction dir) {
         super(type, level, x, y, dir);
-
         this.isMale = isMale;
-
         isChild = true;
-
     }
 
     /**
@@ -107,55 +100,18 @@ public class Rat extends Element {
         return age;
     }
 
-    /**
-     * Make sure all mating conditions between 2 rats are met
-     * Check 2 rats are adults, not sterile of the opposite sex and the female rat is not pregnant
-     * @param rat1 first rat to check sex
-     * @param rat2 second rat to check sex
-     * @return Whether two rats do not have the same sex
-     */
-    public boolean canMate (Rat rat1, Rat rat2) {
-
-        if (rat1.age > 2 && rat2.age > 2) {
-            if (!rat1.isSterile && !rat2.isSterile) {
-                if(rat1.isMale && !rat2.isMale && !rat2.isPregnant) {
-                    rat2.isPregnant = true;
-                    giveBirth(rat2);
-                    return true;
-                } else if (!rat1.isMale && rat2.isMale && !rat1.isPregnant) {
-                    rat1.isPregnant = true;
-                    giveBirth(rat1);
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * @param rat
-     * 
-     * method to create more Rats after Breed() method is called
-     */
-    public void giveBirth(Rat rat) {
-        //TODO After 5 sec change rat isPregnant to false and make baby rats
-        rat.isPregnant = false;
-    }
 
     private void breed() {
         for (Element element : level.getElements(x, y)) {
             if (element.getType().equals(ElementType.Rat)) {
                 Rat rat = (Rat) element;
                 if (rat.isMale == true) {
-                    level.addElementLive(new Rat(ElementType.Rat, level, x, y, Game.random.nextBoolean(), Direction.North));
-                    breeding = BREEDING_TIME;
+                	for(int i =0; i < 3; i++) {
+                		level.addElementLive(new Rat(ElementType.Rat, level, x, y, Game.random.nextBoolean(), Direction.North));
+                	}
+                	breeding = BREEDING_TIME;
                     nextY = y;
-                    nextX = x;
+                    nextX = x;  
                 }
             }
         }
