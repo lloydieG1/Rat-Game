bcef63dd2c4dea593bee7dd0189eadb730df4f87
randleog <willdatank7@gmail.com>
2021-11-30 15:37:37 +0000

    math pi instead of my own

--- src/Element.java
+++ src/Element.java
@@ -304,8 +304,7 @@ public abstract class Element {
 
     protected double cosineInterpolation(double x1, double x2, double min, double max) {
 
-        double pi = 3.14159265;
-        double m2 = (1-Math.cos(min*pi/max))/2;
+        double m2 = (1-Math.cos(min*Math.PI/max))/2;
         return x1+(x2-x1)*m2;
     }
 
