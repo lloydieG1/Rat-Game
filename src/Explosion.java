8dcde20e06f21cdb3d32ba2c0e745c38438eb316
LoriT0T <mesadhq@hotmail.com>
2021-12-01 11:27:40 +0000

    Check Style

--- src/Explosion.java
+++ src/Explosion.java
@@ -3,83 +3,82 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 
-
+/**
+ * Description.
+ *
+ * @author 
+ *
+ */
 public class Explosion extends Element  {
 
+  int maxHealth = 5;
+  private Image blast;
+  private boolean isCenter;
 
 
-    int maxHealth = 5;
+  /**
+   * Description.
+   *
+   * @param type
+   * @param level
+   * @param x
+   * @param y
+   * @param health
+   * @param dir
+   * @param isCenter
+   */
+  public Explosion(ElementType type, Level level, int x, int y, int health,
+                   Direction dir, boolean isCenter) {
+    super(type, level, x, y, dir, health);
 
-    private Image blast;
-    private boolean isCenter;
-
-
-
-    public Explosion(ElementType type, Level level, int x, int y, int health,Direction dir, boolean isCenter) {
-        super(type, level, x, y, dir, health);
-
-        if (isCenter) {
-            blast = ImageLoader.getImage("blastCenter.png", 32);
-        } else {
-            blast = ImageLoader.getImage("blast.png", 32);
-        }
-
-        tickSpeed=Game.FPS/15;
-
-        lastDir=dir;
-        this.isCenter = isCenter;
-
+    if (isCenter) {
+      blast = ImageLoader.getImage("blastCenter.png", 32);
+    } else {
+      blast = ImageLoader.getImage("blast.png", 32);
     }
+    tickSpeed = Game.FPS / 15;
+    lastDir = dir;
+    this.isCenter = isCenter;
+  }
 
-    protected void tick() {
-        for (Element element : level.getElements(x, y)) {
-            if (element.getType().equals(ElementType.Rat)) {
-                level.removeElement(element);
-                Game.score = Game.score+1;
-
-            }
-        }
-        currentTick++;
-        if(currentTick > tickSpeed) {
-            currentTick = 0;
-
-            logic();
-        }
-
+  protected void tick() {
+    for (Element element : level.getElements(x, y)) {
+      if (element.getType().equals(ElementType.Rat)) {
+        level.removeElement(element);
+        Game.score = Game.score + 1;
+      }
     }
-
-    private void logic() {
-        health--;
-        if (health <= 0) {
-            level.removeElement(this);
-        }
+    currentTick++;
+    if (currentTick > tickSpeed) {
+      currentTick = 0;
+      logic();
     }
+    
+  }
 
-
-
-    protected void render(GraphicsContext g) {
-        double size = Game.gameSize;
-
-
-        //get the current interpolated frame positions of rat.
-        double x = renderX();
-        double y = renderY();
-        //calculating the position the rat should be in this frame
-
-
-
-        g.setFill(Color.color(1-(health*0.5/maxHealth),(health*0.9/maxHealth),0));
-        g.fillRect(x, y, size, size);
-
-
+  private void logic() {
+    health--;
+    if (health <= 0) {
+      level.removeElement(this);
     }
-
-    @Override
-    protected String extraInfo() {
-
-        return "," +isCenter;
-    }
+  }
 
 
 
+  protected void render(GraphicsContext g) {
+    double size = Game.gameSize;
+
+    //get the current interpolated frame positions of rat.
+    double x = renderX();
+    double y = renderY();
+    
+    //calculating the position the rat should be in this frame
+    g.setFill(Color.color(1 - (health * 0.5 / maxHealth), (health * 0.9 / maxHealth), 0));
+    g.fillRect(x, y, size, size);
+  }
+
+  @Override
+  protected String extraInfo() {
+    return "," + isCenter;
+  }
 }
