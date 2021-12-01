62efaf0d32d73d35c5eab0f94fe24d07275b6697
randleog <willdatank7@gmail.com>
2021-12-01 19:13:54 +0000

    bomb countdown changes colour

--- src/Bomb.java
+++ src/Bomb.java
@@ -1,5 +1,6 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -130,6 +131,8 @@ public class Bomb extends Element  {
     //calculating the position the rat should be in this frame
     g.drawImage(image, x, y, size, size);
     g.setFont(new Font("monospace", size / 3.2));
+
+      g.setFill(Color.color(1-(health *1.0/ BLAST_TIME), (health * 1.0 / BLAST_TIME), 0));
     g.fillText(Integer.toString(health), x + size / 2.3, y + size / 1.4);
   }
 }
