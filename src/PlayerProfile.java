6c8cdac17aeccf40c3cc84bc30d9c42d7d78cf27
LoriT0T <mesadhq@hotmail.com>
2021-12-01 13:04:08 +0000

    CheckStyle changes

--- src/PlayerProfile.java
+++ src/PlayerProfile.java
@@ -1,42 +1,54 @@
 /**
- * @author lloyd
+ * Description.
+ *
+ * @author Lloyd
  */
 public class PlayerProfile {
-	private static final String PROFILE_FILE_PATH = "res/profiles/";
+  private static final String PROFILE_FILE_PATH = "res/profiles/";
 	
-	private String username;
-	private int maxLevel;
-	private int highscore;
-	private String playerProfilePath;
+  private String username;
+  private int maxLevel;
+  private int highscore;
+  private String playerProfilePath;
 	
-	public PlayerProfile(String username, int maxLevel) {
-		this.username = username;
-		this.maxLevel = maxLevel;
-		this.playerProfilePath = PROFILE_FILE_PATH + username + ".txt";
-		
-	}
+  /**
+   * Description.
+   *
+   * @param username
+   * @param maxLevel
+   */
+  public PlayerProfile(String username, int maxLevel) {
+    this.username = username;
+    this.maxLevel = maxLevel;
+    this.playerProfilePath = PROFILE_FILE_PATH + username + ".txt";
+  }
 	
-	public int getMaxLevel() {
-		return maxLevel;
-	}
-	public void setMaxLevel(int maxLevel) {
-		this.maxLevel = maxLevel;
-	}
-	public int getHighscore() {
-		return highscore;
-	}
-	public void setHighscore(int highscore) {
-		this.highscore = highscore;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public int getMaxLevel() {
+    return maxLevel;
+  }
+  
+  public void setMaxLevel(int maxLevel) {
+    this.maxLevel = maxLevel;
+  }
+  
+  public int getHighscore() {
+    return highscore;
+  }
+  
+  public void setHighscore(int highscore) {
+    this.highscore = highscore;
+  }
+  
+  public String getUsername() {
+    return username;
+  }
+  
+  public void setUsername(String username) {
+    this.username = username;
+  }
 	
-	public String getPlayerProfilePath() {
-		return playerProfilePath;
-	}
+  public String getPlayerProfilePath() {
+    return playerProfilePath;
+  }
 	
 }
