8f9131f3984f45b86a11d8a1319cd69fb0adb88a
LoriT0T <mesadhq@hotmail.com>
2021-12-01 13:00:09 +0000

    CheckStyle

--- src/MaleChanger.java
+++ src/MaleChanger.java
@@ -3,67 +3,69 @@ import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
 
 
-
+/**
+ * Description.
+ *
+ * @author 
+ *
+ */
 public class MaleChanger extends Element {
 	
-	private Image image;
-	
-	private int maleChangeCount;
-	
-	
+  private Image image;
+  private int maleChangeCount;	
 
-	public MaleChanger(ElementType type, Level level, int x, int y, int health) {
-		super(type, level, x, y, Direction.North, health);
-		// TODO Auto-generated constructor stub
-		
-		maleChangeCount = 0;
-		
-		image = ImageLoader.getImage("maleGenderChange.png", 64);
-		
-	}
+  /**
+   * Description.
+   *
+   * @param type
+   * @param level
+   * @param x
+   * @param y
+   * @param health
+   */
+  public MaleChanger(ElementType type, Level level, int x, int y, int health) {
+    super(type, level, x, y, Direction.North, health);
+    // TODO Auto-generated constructor stub
+    maleChangeCount = 0;
+    image = ImageLoader.getImage("maleGenderChange.png", 64);
+  }
 	
 	
-	/**
-     * Runs Tick() behaviours, allowing them to run logics automatically 
-     * in time with other elements
-     */
-	@Override
-	protected void tick() {
-        if (level.getTile(x,y).getType().equals(TileType.Grass)) {
-            level.removeElement(this);
-            System.out.println("deleting male changer");
-        }
-        currentTick++;
-        if(currentTick > tickSpeed) {
-            currentTick = 0;
-            logic();
-        }
-
+  /**
+   * Runs Tick() behaviours, allowing them to run logics automatically 
+   * in time with other elements.
+   */
+  @Override
+  protected void tick() {
+    if (level.getTile(x, y).getType().equals(TileType.Grass)) {
+      level.removeElement(this);
+      System.out.println("deleting male changer");
     }
-	
-	/**
-     * the logic of the Element
-     */
-    private void logic() {
-        if (this.maleChangeCount == 1){
-        	level.removeElement(this);
-        }
+    currentTick++;
+    if (currentTick > tickSpeed) {
+      currentTick = 0;
+      logic();
     }
-    
-
-
+  }
 	
-	/**
-     * renders a Male Changer sign
-     * @param g graphics context
-     */
-    @Override
-    protected void render(GraphicsContext g) {
-        double x = renderX();
-        double y = renderY();
-
-        g.drawImage(image,x, y, size, size);
-
+  /**
+   * The logic of the Element.
+   */
+  private void logic() {
+    if (this.maleChangeCount == 1) {
+      level.removeElement(this);
     }
-
+  }
+	
+  /**
+   * Renders a Male Changer sign.
+   *
+   * @param g graphics context
+   */
+  @Override
+  protected void render(GraphicsContext g) {
+    double x = renderX();
+    double y = renderY();
+    g.drawImage(image, x, y, size, size);
+  }
 }
