47056d3afa0dcee4ed02e080f7f5516cf070a32b
LoriT0T <mesadhq@hotmail.com>
2021-12-01 11:18:49 +0000

    Check style

--- src/ElementType.java
+++ src/ElementType.java
@@ -2,7 +2,7 @@
  * This enum contains each Element type for use whenever
  * a method needs to know what it is interacting with.
  *
- * @author William Randle, Mosead, LLoyd
+ * @author William Randle, Mosaed, LLoyd
  */
 
 public enum ElementType {
