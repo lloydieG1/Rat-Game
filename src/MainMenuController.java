c43dedf90474d3caeafdd15bbfd6c2392fbac70c
randleog <willdatank7@gmail.com>
2021-12-03 16:26:40 +0000

    daily message scrolls

--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -1,8 +1,14 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,6 +22,7 @@ import java.util.ResourceBundle;
 public class MainMenuController implements Initializable {
 			
 
+    String oldMotd = "";
 	String motd = ""; //text on screen for message of the day
 
     @FXML
@@ -23,7 +30,11 @@ public class MainMenuController implements Initializable {
 	
 	@FXML
 	Text currentUser; //user that is logged in
-	
+
+    private Timeline swap;
+
+    private double text1Pos; //daily new message position on the screen
+    private double text2Pos; //daily old message position on the screen
 
 	public void setProfileText() {
 		currentUser.setText("Current User: " + Game.currentProfile.getUsername());
@@ -54,8 +65,9 @@ public class MainMenuController implements Initializable {
 	}
 
     public void refreshDailyMessage() {
-        GraphicsContext g = motdCanvas.getGraphicsContext2D();
-
+        text1Pos = 0;
+        text2Pos = (int)motdCanvas.getWidth();
+        oldMotd = motd;
         try {
             String newMessage = DailyMessage.getMessage(null);
 
@@ -66,9 +78,24 @@ public class MainMenuController implements Initializable {
             ex.printStackTrace();
         }
 
-        g.fillText(motd, 0,20);
+        swap.play();
 
 
+
+    }
+
+    private void renderMessage(GraphicsContext g) {
+        double moveWidth = motdCanvas.getWidth()/Game.FPS;
+
+
+        g.setFill(Color.color(0.6,0.6,0.6));
+        g.fillRect(0,0, motdCanvas.getWidth(), motdCanvas.getHeight());
+        text1Pos-= moveWidth;
+        text2Pos-= moveWidth;
+        g.setFill(Color.color(1,0.8,0));
+        g.setFont(new Font(20));
+        g.fillText(oldMotd, text1Pos,20);
+        g.fillText(motd, text2Pos,20);
     }
 
 
@@ -80,8 +107,14 @@ public class MainMenuController implements Initializable {
 	 */
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
+        int fpstime = 1000/Game.FPS;
+        swap = new Timeline(new KeyFrame(Duration.millis(fpstime), (ActionEvent event) -> {
+            renderMessage(motdCanvas.getGraphicsContext2D());
+        }));
+        swap.setCycleCount(Game.FPS);
+
         Game.mainMenuController = this;
-		refreshDailyMessage();
+
 	}
 }
 
