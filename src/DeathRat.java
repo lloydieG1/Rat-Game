297aa48e7f35f900190e3dc31b335a07a1390187
randleog <willdatank7@gmail.com>
2021-12-04 20:44:37 +0000

    deathrats move as fast as adult rats

--- src/DeathRat.java
+++ src/DeathRat.java
@@ -32,7 +32,6 @@ public class DeathRat extends Element {
     // TODO Auto-generated constructor stub
     ratsKilled = 0;
     image = ImageLoader.getImage("deathRat.png", 64);
-    tickSpeed = Game.FPS / 2;
   }
 
   private void logic() {
